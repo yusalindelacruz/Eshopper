966c65bf05651639531836b290c1bef9714ecf0b
yusalindelacruz <yusalindelacruz765@gmail.com>
2024-11-29 15:46:25 -0600

    Add files via upload
    EshooperL FINAL

--- shop.php
+++ shop.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Software Punto de Venta" content="">
-    <meta name="Gerardo Aguilar" content="">
+    <meta name="Lesly Mercado Yusalin De La Cruz" content="">
     <title>Comprar | E-Shopper</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
